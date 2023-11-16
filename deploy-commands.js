561991a4ac84574740624bf05ea495646f61c0ac
Rayven <ravenistaken@proton.me>
2023-11-16 22:09:32 +0500

    make all global

--- deploy-commands.js
+++ deploy-commands.js
@@ -36,7 +36,7 @@ const rest = new REST().setToken(process.env.TOKEN);
 
 		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
-			Routes.applicationGuildCommands(process.env.C_ID, process.env.G_ID),
+			Routes.applicationCommands(process.env.C_ID),
 			{ body: commands },
 		);
 
